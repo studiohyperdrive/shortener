3d79435ceb9f238f520d5eccb93b08f68bf94a0f
Enzo Eghermanne <enzo.eghermanne@district01.be>
2018-04-22 22:56:38 +0200

    enabled rollup-uglify

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import uglify from "rollup-plugin-uglify";
 
 export default [
 	{
-		// plugins: [uglify()],
+		plugins: [uglify()],
 		input: "src/index.js",
 		output: [
 			{ file: pkg.main, format: "cjs" },
